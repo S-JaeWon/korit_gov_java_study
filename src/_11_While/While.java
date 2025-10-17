de0b3ddb27bef0d314aca131b1977613e69d9787
S-JaeWon <ssm5170@gmail.com>
2025-10-17 16:50:10 +0900

    Feat: ì¡while문

---
+++ src/_11_While/While.java
@@ -0,0 +1,98 @@
+package _11_While;
+
+import java.util.Scanner;
+
+public class While {
+    public static void main(String[] args) {
+        // 반복문
+        // 조건이 참일 경우 반복
+        // for문 -> 회수
+        // while문 -> 조건
+
+        /*
+        * while(조건) {
+        *   실행할 코드
+        * }
+        * */
+
+        // 1 ~ 5 까지 출력
+//        int i = 1;
+//
+//        while (i <= 5) {
+//            System.out.println(i++);
+//        }
+//
+//        i = 10;
+//        while (i >= 1) {
+//            System.out.println(i--);
+//        }
+//
+//        // 짝수만 출력
+//        i = 1;
+//        while (i <= 10){
+//            if (i % 2 ==0) {
+//                System.out.println(i);
+//            }
+//            i++;
+//        }
+//
+//        boolean flag = true;
+        Scanner scanner = new Scanner(System.in);
+//
+//        while (flag) {
+//            int score = scanner.nextInt();
+//            if (0 <= score && score <= 100) {
+//                System.out.println("score : " + score);
+//                flag = false;
+//            } else {
+//                System.out.println("유효하지 않은 점수입니다.");
+//            }
+//        }
+
+//        int menu;
+//
+//        while (true) {
+//            System.out.println("메뉴를 선택하세요.");
+//            System.out.println("1. 주문하기");
+//            System.out.println("2. 취소하기");
+//            System.out.println("3. 재주문하기");
+//            System.out.print("메뉴를 고르세요 -> ");
+//            menu = scanner.nextInt();
+//
+//            switch (menu) {
+//                case 1:
+//                    System.out.println("메뉴주문");
+//                    break;
+//                case 2:
+//                    System.out.println("취소");
+//                    break;
+//                case 3:
+//                    System.out.println("재주문");
+//                    break;
+//                default:
+//                    System.out.println("다시 골라주세요");
+//                    continue;
+//            }
+//            break;
+//        }
+
+        // 1 ~ 100의 합
+        int num = 0;
+        int sum = 0;
+
+        while (true) {
+            sum += num;
+            ++num;
+            if (num > 100) {
+                break;
+            }
+        }
+        System.out.println(sum);
+
+        // 1 ~ 100
+        // 10일때 메뉴 sold out
+        // sold out 이면 가게 폐점
+        // 폐점 이후 영업 종료 출력
+
+    }
+}
