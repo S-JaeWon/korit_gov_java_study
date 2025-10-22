7b1d4c8e7f60b4987e9d9effa47a17f661aa9423
S-JaeWon <ssm5170@gmail.com>
2025-10-22 16:49:50 +0900

    Feat: 코테 연습

---
+++ src/_00_CodeTest/ex1.java
@@ -0,0 +1,19 @@
+package _00_CodeTest;
+
+import java.util.Arrays;
+
+public class ex1 {
+    public static void main(String[] args) {
+        String A = "hello";
+        String B = "ohell";
+
+        String[] str = new String [A.length()];
+
+        for (int i = 0; i < A.length(); i++) {
+            str[i] = String.valueOf(A.charAt(i));
+        }
+        System.out.println(Arrays.toString(str));
+
+
+    }
+}
